b690b2e83ecdb931a2b3c92ddbd8d09a457a4ff1
a1038006 <a1038006@AL-CLOGUEW8>
2017-01-31 16:08:32 +0000

    Add project files.

---
+++ Print Application/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Print_Application
+{
+    partial class frmPrintApplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVBID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnPrint.Location = new System.Drawing.Point(275, 0);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VBID:";
+            // 
+            // txtVBID
+            // 
+            this.txtVBID.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtVBID.Location = new System.Drawing.Point(35, 0);
+            this.txtVBID.Name = "txtVBID";
+            this.txtVBID.Size = new System.Drawing.Size(240, 20);
+            this.txtVBID.TabIndex = 2;
+            // 
+            // frmPrintApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 23);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.txtVBID);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPrintApplication";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPrintApplication_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtVBID;
+    }
+}
+
---
+++ Print Application/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//  Calls in the functions from our libraries
+using SchraderElectronics.SEL_InterfaceLibrary;
+
+namespace Print_Application
+{
+    public partial class frmPrintApplication : Form
+    {
+        public frmPrintApplication()
+        {
+            InitializeComponent();
+            //If you need something to happen before the form loads - put it here
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            //  Print function
+            long _asidID = 0;
+            try
+            {
+                //  Get the asic id from the valvebody id passed in
+                _asidID = GetAsicID(txtVBID.Text);
+
+                if (_asidID == 0)
+                {
+                    //  No asic ID
+                    throw new Exception(string.Format("Error receiving ASICID for VBID:{0}", txtVBID.Text));
+                }
+
+                //  Builds the ZPL string
+                string _zpl = BuildZPL(txtVBID.Text, _asidID);
+                //  insert printer name
+                string _printer = "ZPL Printer Name";
+
+                //  Call print function
+                if (!PrintLabel(_zpl, _printer))
+                {
+                    //  If false - something went wrong
+                    throw new Exception(string.Format("Error with printing for VBID:{0}", txtVBID.Text));
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void frmPrintApplication_Load(object sender, EventArgs e)
+        {
+            //  Need something to run on load? put it here
+        }
+
+        private bool PrintLabel(string _zpl, string _printer)
+        {
+            clsRawPrinterHelper.SendStringToPrinter(_printer, _zpl);
+            return true;
+        }
+
+        private long GetAsicID(string _vbid)
+        {
+            //  Initialise variables - Insert 
+            string _ssql = string.Format("", _vbid);
+            long _response = 0;
+            SqlConnection _connection = null;
+            SqlCommand _command = null;
+            SqlDataReader _dataReader = null;
+
+            try
+            {
+                //  Assign the connection to the central SQL Connection which pulls the connection string from the local XML configurataion (app.config)
+                using (_connection = clsDataConnections.GetSQLConnection(clsDataConnections.Connection.Central))
+                {
+                    //Opens the connection
+                    _connection.Open();
+                    //  Create the command - can be reader, update, insert, etc.
+                    using (_command = new SqlCommand(_ssql, _connection))
+                    {
+                        //  Create the reader to pull out the data
+                        _dataReader = _command.ExecuteReader();
+                        while (_dataReader.Read())
+                        {
+                            if (!_dataReader.HasRows)
+                            {
+                                //  When the query has no rows, throw this.
+                                throw new Exception(String.Format("Query provides no rows for VBID:{0}", _vbid));
+                            }
+                            else
+                            {
+                                //  Gets the first value as an Int32 
+                                _response = _dataReader.GetInt32(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                //  throw up a level
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //  throw up a level
+                throw ex;
+            }
+            finally
+            {
+                //  clean up after yourself
+                _dataReader.Close();
+                _dataReader.Dispose();
+                _command.Dispose();
+                _connection.Close();
+                _connection.Dispose();
+            }
+
+            //  Return what you sent
+            return _response;
+        }
+
+        private string BuildZPL(string _vbid, long _asic)
+        {
+            //Build up the ZPL here - insert as needed
+            return string.Format("", _vbid, _asic);
+        }
+    }
+}
